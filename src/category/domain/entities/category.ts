bbb68c72ccb7b20bbc99ae88f162557c58b37563
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2023-04-17 21:35:42 -0300

    add category validation

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,4 @@
+import ValidatorRules from "../../../@seedwork/validators/validator-rules";
 import Entity from "../../../@seedwork/domain/entity/entity";
 import UniqueEntityId from "../../../@seedwork/domain/value-objects/unique-entity-id.vo";
 
@@ -10,6 +11,7 @@ export type CategoryProperties = {
 export default class Category extends Entity<CategoryProperties> {
 
   constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    Category.validate(props)
     super(props, id)
     this.description = this.props.description;
     this.is_active = this.props.is_active;
@@ -17,8 +19,15 @@ export default class Category extends Entity<CategoryProperties> {
   }
 
   update(name: string, description: string): void {
+    Category.validate({ name, description, is_active: this.is_active })
     this.name = name;
     this.description = description
+  }
+
+  static validate(props: Omit<CategoryProperties, 'created_at'>) {
+    ValidatorRules.values(props.name, "name").required().string()
+    ValidatorRules.values(props.description, "description").string()
+    ValidatorRules.values(props.is_active, "is_active").boolean()
   }
 
   activate() {
