35ab3b2acf7b1ed7ddde1ba91b9e1b642a9e8574
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2023-05-08 20:57:46 -0300

    category repository

---
+++ src/category/domain/repository/category.repository.ts
@@ -0,0 +1,6 @@
+import { RepositoryInterface } from "../../../@seedwork/domain/repository/repository-contracts";
+import Category from "../entities/category";
+
+export default interface CategoryRepository extends RepositoryInterface<Category> {
+
+}
---
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -0,0 +1,9 @@
+import InMemoryRepository from "../../../@seedwork/domain/repository/in-memory.repository";
+import Category from "../../domain/entities/category";
+import CategoryRepository from "../../domain/repository/category.repository";
+
+export default class CategoryInMemoryRepository
+    extends InMemoryRepository<Category>
+    implements CategoryRepository {
+
+}
